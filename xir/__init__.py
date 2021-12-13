a6486bc1495130bd53aceefde378f4b29347540a
Samuel Banning <samcbanning@gmail.com>
2021-12-13 11:31:31 -0500

    fixed linting

--- xir/__init__.py
+++ xir/__init__.py
@@ -48,7 +48,7 @@ def parse_script(script: str, debug: bool = False, **kwargs) -> Program:
         )
         tree = parser.parse(script)
         return Transformer(**kwargs).transform(tree)
-    
+
     parser = lark.Lark(
         grammar=_read_lark_file(),
         maybe_placeholders=True,
